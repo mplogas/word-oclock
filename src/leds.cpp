5f23907976842e614fc500a2351bc558f581829e
Marc Plogas <marc.plogas@digitalnatives.de>
2024-11-28 22:29:32 +0100

    led refactoring pt2

--- src/leds.cpp
+++ src/leds.cpp
@@ -1,7 +1,7 @@
 #include "leds.h"
 
 LED::LED() : brightness(DEFAULT_BRIGHTNESS), color(CRGB::White) {
-    FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
+    FastLED.addLeds<WS2812B, DATA_PIN, GRB>(this->leds, NUM_LEDS);
     FastLED.setBrightness(brightness);
 }
 
@@ -10,6 +10,7 @@ LED::~LED() {
 }
 
 void LED::setBrightness(uint8_t brightness) {
+    setAutoBrightness(false);
     this->brightness = brightness;
     FastLED.setBrightness(brightness);
     FastLED.show();
@@ -21,25 +22,60 @@ void LED::setColor(const CRGB& color) {
     FastLED.show();
 }
 
-void LED::toggleLEDs(const std::vector<std::pair<int, int>>& ledRanges) {
+void LED::setAutoBrightness(bool autoBrightness, int illuminanceThresholdHigh, int illuminanceThresholdLow) {
+    this->autoBrightness = autoBrightness;
+    if(autoBrightness) {
+        this->illuminanceThresholdHigh = illuminanceThresholdHigh;
+        this->illuminanceThresholdLow = illuminanceThresholdLow;
+        handleAutoBrightness();
+    } else {
+        setBrightness(DEFAULT_BRIGHTNESS);
+        this->illuminanceThresholdHigh = ILLUMINANCE_THRESHOLD_HIGH;
+        this->illuminanceThresholdLow = ILLUMINANCE_THRESHOLD_LOW;
+    }
+}
+
+void LED::registerIlluminanceSensorCallback(const IlluminanceSensorCallback &callback) {
+    sensorCallback = callback;
+}
+
+void LED::setLEDs(const std::vector<std::pair<int, int>>& ledRanges) {
+
+    fill_solid(this->leds, NUM_LEDS, CRGB::Black);
+
     for (const auto& range : ledRanges) {
         int start = range.first;
         int count = range.second;
         for (int i = start; i < start + count && i < NUM_LEDS; ++i) {
-            leds[i] = leds[i] ? CRGB::Black : color;
+            this->leds[i] = this->color;
         }
     }
     FastLED.show();
 }
 
-void LED::handleAutoBrightness(int illuminanceThresholdHigh, int illuminanceThresholdLow) {
-    if(!autoBrightness) {
+void LED::clearLEDs() {
+    fill_solid(leds, NUM_LEDS, CRGB::Black);
+    FastLED.show();
+}
+
+String LED::RGBtoHex(const CRGB& color) {
+    char hex[7];
+    sprintf(hex, "%02X%02X%02X", color.r, color.g, color.b);
+    return String(hex);
+}
+
+CRGB LED::HexToRGB(const String& hex) {
+    long number = strtol(hex.c_str(), nullptr, 16);
+    return CRGB(number >> 16, number >> 8 & 0xFF, number & 0xFF);
+}
+
+void LED::handleAutoBrightness() {
+    if(!this->autoBrightness) {
         return;
     }
 
     int illuminance = analogRead(LDR_PIN);
-
-    int targetBrightness = map(illuminance, illuminanceThresholdLow, illuminanceThresholdHigh, BRIGHTNESS_MIN, BRIGHTNESS_MAX);
+    int targetBrightness = map(illuminance, this->illuminanceThresholdLow, this->illuminanceThresholdHigh, BRIGHTNESS_MIN, BRIGHTNESS_MAX);
     targetBrightness = constrain(targetBrightness, BRIGHTNESS_MIN, BRIGHTNESS_MAX);
 
     /*
@@ -49,21 +85,21 @@ void LED::handleAutoBrightness(int illuminanceThresholdHigh, int illuminanceThre
         A smaller alpha results in smoother changes but slower responsiveness.
         An alpha of 0.1 means the new brightness is 10% of the target and 90% of the current brightness.
     */
-    const float alpha = 0.1;  // Smoothing factor (0 < alpha <= 1)
-    uint8_t smoothedBrightness = brightness * (1 - alpha) + targetBrightness * alpha; // Exponential moving average
+    const float alpha = 0.2;  // Smoothing factor (0 < alpha <= 1)
+    uint8_t smoothedBrightness = this->brightness * (1 - alpha) + targetBrightness * alpha; // Exponential moving average
     setBrightness(smoothedBrightness);
 }
 
 void LED::loop() {
-
     unsigned long currentMillis = millis();
-    if(currentMillis - lastBrightnessUpdate > BRIGHTNESS_UPDATE_INTERVAL && autoBrightness) {
-        lastBrightnessUpdate = currentMillis;
+    if(currentMillis - this->lastBrightnessUpdate > BRIGHTNESS_UPDATE_INTERVAL && this->autoBrightness) {
+        this->lastBrightnessUpdate = currentMillis;
         handleAutoBrightness();
     }
-    if(currentMillis - lastSensorUpdate > ILLUMINANCE_INTERVAL && sensorCallback) {
-        lastSensorUpdate = currentMillis;
+    if(currentMillis - this->lastSensorUpdate > ILLUMINANCE_INTERVAL && this->sensorCallback) {
+        this->lastSensorUpdate = currentMillis;
         int illuminance = analogRead(LDR_PIN);
         sensorCallback(illuminance);
     }
+
 }
--- src/leds.h
+++ src/leds.h
@@ -12,15 +12,19 @@ using IlluminanceSensorCallback = std::function<void(const int value)>;
 
 class LED {
 public:
+    static constexpr int ILLUMINANCE_THRESHOLD_HIGH = 10000; // High illuminance threshold
+    static constexpr int ILLUMINANCE_THRESHOLD_LOW = 300; // Low illuminance threshold
     LED();
     ~LED();
     void setBrightness(uint8_t brightness);
     void setColor(const CRGB& color);
-    void toggleLEDs(const std::vector<std::pair<int, int>>& ledRanges);
-    void setAutoBrightness(bool autoBrightness);
-    void setAutoBrightness(bool autoBrightness, int illuminanceThresholdHigh, int illuminanceThresholdLow);
+    void setLEDs(const std::vector<std::pair<int, int>>& ledRanges);
+    void clearLEDs();
+    void setAutoBrightness(bool autoBrightness, int illuminanceThresholdHigh =  ILLUMINANCE_THRESHOLD_HIGH, int illuminanceThresholdLow = ILLUMINANCE_THRESHOLD_LOW);
     void registerIlluminanceSensorCallback(const IlluminanceSensorCallback &callback);
     void loop();
+    static String RGBtoHex(const CRGB& color);
+    static CRGB HexToRGB(const String& hex);
 private:
     static const int NUM_LEDS = 9; // Number of LEDs (should be 121)
     static const int DATA_PIN = 25; // LED data pin
@@ -29,18 +33,18 @@ private:
     static const uint8_t BRIGHTNESS_MIN = 50;
     static const uint8_t BRIGHTNESS_MAX = 255;
     static const unsigned long BRIGHTNESS_UPDATE_INTERVAL = 500; // Brightness update interval
-    static const int ILLUMINANCE_THRESHOLD_HIGH = 10000; // High illuminance threshold
-    static const int ILLUMINANCE_THRESHOLD_LOW = 300; // Low illuminance threshold
     static const int ILLUMINANCE_INTERVAL = 500; // Illuminance update interval
     CRGB leds[NUM_LEDS];
     uint8_t brightness;
     bool autoBrightness;
+    int illuminanceThresholdHigh;
+    int illuminanceThresholdLow;
     CRGB color;
     uint8_t smoothedBrightness;
     unsigned long lastBrightnessUpdate;
     unsigned long lastSensorUpdate;
     IlluminanceSensorCallback sensorCallback;
-    void handleAutoBrightness(int illuminanceThresholdHigh = ILLUMINANCE_THRESHOLD_HIGH, int illuminanceThresholdLow = ILLUMINANCE_THRESHOLD_LOW);
+    void handleAutoBrightness();
 };
 
 #endif // LEDS_H

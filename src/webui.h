8f0c92acda23246e83a6ecfd11fe7f385cc21062
Marc Plogas <mplogas@users.noreply.github.com>
2025-02-08 00:29:41 +0100

    moved to progmem for param & value keys (#2)

--- src/webui.h
+++ src/webui.h
@@ -13,7 +13,7 @@
 #include "callbacktypes.h"
 
 using RequestCallback = std::function<void(ControlType type, const std::map<String, String>& params)>;
-using ResponseCallback = std::function<const std::map<String, String>(PageType page)>;
+using ResponseCallback = std::function<std::map<String, String>(PageType page)>;
 using UpdateCallback = std::function<void(UpdateType type, const String &filename, size_t index, uint8_t *data, size_t len, bool final)>;
 using UpdateSuccessCallback = std::function<bool()>;
 
@@ -32,7 +32,6 @@ class WebUI
         String systemPageProcessor(const String &var, const std::map<String, String> &params);
         String firmwarePageProcessor(const String &var, const std::map<String, String> &params);
         String headerProcessor(PageType page);
-        //String includeProcessor(const String &var);
         String pageProcessor(const String &var, PageType page, const std::map<String, String> &params);
 
         // Helper functions
@@ -46,77 +45,79 @@ class WebUI
         void printAllParams(AsyncWebServerRequest *request);
         String readFile(const char* path);
 
+        // TODO: move to PROGMEM!
+
         // paths
-        static constexpr const char* PATH_NAVIGATION_HTML = "/navigation.html";
-        static constexpr const char* PATH_LIGHT_HTML = "/light.html";
-        static constexpr const char* PATH_TIME_HTML = "/time.html";
-        static constexpr const char* PATH_SYSTEM_HTML = "/system.html";
-        static constexpr const char* PATH_FIRMWARE_HTML = "/firmware.html";
-        static constexpr const char* PATH_WIFI_HTML = "/wifimanager.html";
+        static const char PATH_NAVIGATION_HTML[] PROGMEM;
+        static const char PATH_LIGHT_HTML[] PROGMEM;
+        static const char PATH_TIME_HTML[] PROGMEM;
+        static const char PATH_SYSTEM_HTML[] PROGMEM;
+        static const char PATH_FIRMWARE_HTML[] PROGMEM;
+        static const char PATH_WIFI_HTML[] PROGMEM;
         static constexpr const char* PATH_CSS = "/style.css";
         static constexpr const char* PATH_JS = "/index.js";
         static constexpr const char* PATH_ICON = "/favicon.ico";
 
         // page titles
-        static constexpr const char* LIGHT_PAGE_TITLE = "Light Settings";
-        static constexpr const char* SYSTEM_PAGE_TITLE = "System Configuration";
-        static constexpr const char* TIME_PAGE_TITLE = "Time Configuration";
-        static constexpr const char* FIRMWARE_PAGE_TITLE = "Firmware Update";
+        static const char LIGHT_PAGE_TITLE[] PROGMEM;
+        static const char SYSTEM_PAGE_TITLE[] PROGMEM;
+        static const char TIME_PAGE_TITLE[] PROGMEM;
+        static const char FIRMWARE_PAGE_TITLE[] PROGMEM;
 
         // processor variables
-        static constexpr const char* PROC_PAGE_TITLE = "PAGE_TITLE";
-        static constexpr const char* PROC_FW_VERS = "FW_VERSION";
+        static const char PROC_PAGE_TITLE[] PROGMEM;
+        static const char PROC_FW_VERS[] PROGMEM;
 
         // values
-        static constexpr const char* VALUE_SUCCESS = "Success";
-        static constexpr const char* VALUE_ERROR = "Error!";
+        static const char VALUE_SUCCESS[] PROGMEM;
+        static const char VALUE_ERROR[] PROGMEM;
         static constexpr const char* VALUE_ACTIVE = "active";
-        static constexpr const char* VALUE_FIRMWARE = "firmware";
-        static constexpr const char* VALUE_FILESYS = "filesystem";
+        static const char VALUE_FIRMWARE[] PROGMEM;
+        static const char VALUE_FILESYS[] PROGMEM;
 
-        static constexpr const char* CONTENT_TEXT = "text/plain";
-        static constexpr const char* CONTENT_HTML = "text/html";
+        static const char CONTENT_TEXT[] PROGMEM;
+        static const char CONTENT_HTML[] PROGMEM;
         static constexpr const char* CONTENT_CACHE = "max-age=3600";
 
-        static constexpr const char* PARAM_FW_Type = "updateType";
+        static const char PARAM_FW_Type[] PROGMEM;
 
     public:
 
-        static constexpr const char* VALUE_ON = "1";
-        static constexpr const char* VALUE_OFF = "0";
+        static const char VALUE_ON[] PROGMEM;
+        static const char VALUE_OFF[] PROGMEM;
 
-        static constexpr const char* PARAM_WIFI_SSID = "ssid";
-        static constexpr const char* PARAM_WIFI_PASS = "wifi-pass";
-        static constexpr const char* PARAM_ENABLED = "enabled";
-        static constexpr const char* PARAM_OPTION = "option";
-        static constexpr const char* PARAM_VALUE = "value";
-        static constexpr const char* PARAM_COLOR = "color";
-        static constexpr const char* PARAM_BRIGHTNESS = "brightness";
-        static constexpr const char* PARAM_AUTO_BRIGHTNESS_ENABLED = "abEnabled";
-        static constexpr const char* PARAM_TIME = "time";
-        static constexpr const char* PARAM_BROKER_ENABLED = "mqttEnabled";
-        static constexpr const char* PARAM_BROKER_HOST = "mqttHost";
-        static constexpr const char* PARAM_BROKER_PORT = "mqttPort";
-        static constexpr const char* PARAM_BROKER_USER = "mqttUsername";
-        static constexpr const char* PARAM_BROKER_PASS = "mqttPassword";
-        static constexpr const char* PARAM_BROKER_DEFAULT_TOPIC = "mqttTopic";
-        static constexpr const char* PARAM_NTP_ENABLED = "ntpEnabled";
-        static constexpr const char* PARAM_NTP_HOST = "ntpHost";
-        static constexpr const char* PARAM_NTP_UPDATE_INTERVAL = "ntpInterval";
-        static constexpr const char* PARAM_NTP_TIMEZONE = "ntpTimezone";
-        static constexpr const char* PARAM_SCHEDULE_ENABLED = "scheduleEnabled";
-        static constexpr const char* PARAM_SCHEDULE_START = "scheduleStart";
-        static constexpr const char* PARAM_SCHEDULE_END = "scheduleEnd";
-        static constexpr const char* PARAM_CLOCKFACE = "clockFace";        
-        static constexpr const char* PARAM_CLOCKFACE_OPTION = "clockFaceOption";
-        static constexpr const char* PARAM_FW_VERSION = "fwVersion";
+        static const char PARAM_WIFI_SSID[] PROGMEM;
+        static const char PARAM_WIFI_PASS[] PROGMEM;
+        static const char PARAM_ENABLED[] PROGMEM;
+        static const char PARAM_OPTION[] PROGMEM;
+        static const char PARAM_VALUE[] PROGMEM;
+        static const char PARAM_COLOR[] PROGMEM;
+        static const char PARAM_BRIGHTNESS[] PROGMEM;
+        static const char PARAM_AUTO_BRIGHTNESS_ENABLED[] PROGMEM;
+        static const char PARAM_TIME[] PROGMEM;
+        static const char PARAM_BROKER_ENABLED[] PROGMEM;
+        static const char PARAM_BROKER_HOST[] PROGMEM;
+        static const char PARAM_BROKER_PORT[] PROGMEM;
+        static const char PARAM_BROKER_USER[] PROGMEM;
+        static const char PARAM_BROKER_PASS[] PROGMEM;
+        static const char PARAM_BROKER_DEFAULT_TOPIC[] PROGMEM;
+        static const char PARAM_NTP_ENABLED[] PROGMEM;
+        static const char PARAM_NTP_HOST[] PROGMEM;
+        static const char PARAM_NTP_UPDATE_INTERVAL[] PROGMEM;
+        static const char PARAM_NTP_TIMEZONE[] PROGMEM;
+        static const char PARAM_SCHEDULE_ENABLED[] PROGMEM;
+        static const char PARAM_SCHEDULE_START[] PROGMEM;
+        static const char PARAM_SCHEDULE_END[] PROGMEM;
+        static const char PARAM_CLOCKFACE[] PROGMEM;        
+        static const char PARAM_CLOCKFACE_OPTION[] PROGMEM;
+        static const char PARAM_FW_VERSION[] PROGMEM;
         
         WebUI(AsyncWebServer &server);
         ~WebUI();
         void init(const RequestCallback &requestCb, 
-        const ResponseCallback &responseCb,
-        const UpdateCallback &updateCb, 
-        const UpdateSuccessCallback &updateSuccessCb);
+                  const ResponseCallback &responseCb,
+                  const UpdateCallback &updateCb, 
+                  const UpdateSuccessCallback &updateSuccessCb);
         void initHostAP(const RequestCallback &wrequestCb);
 };
 
